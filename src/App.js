f02adef037d323de8ce5c24748c036e177d6dcfc
Dwi Pasca <dpnursanti@gmail.com>
2020-10-23 12:42:45 +0700

    set data user in app.js and set style in App.css

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react'
+import './App.css'
 
-function App() {
+function App () {
+  
+  const [users] = useState([
+    {name:"Dwi Pasca", job:"Web developer"},
+    {name:"Jane Shepard", job:"Soldier"},
+    {name:"Liara T'Soni", job:"Researcher"}
+  ])
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
